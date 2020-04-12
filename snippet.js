45ab69ffb93e194b034bcdcef117303a54b71d52
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-04-12 17:22:53 +0600

    reusable snippet complete

---
+++ snippet.js
@@ -0,0 +1,30 @@
+const getTodos = (resource) => {
+
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+
+            request.addEventListener('readystatechange', () => {
+                if(request.readyState === 4 && request.status === 200) {
+                    const data = JSON.parse(request.responseText);
+                    resolve(data);
+                } else if(request.readyState === 4) {
+                    reject('err getting resource');
+                };
+            }); 
+        
+        request.open('GET', resource); 
+        request.send(); 
+    });
+};
+
+getTodos('luigi.json').then((data) => {
+    console.log('promise 1 resolved:', data);
+    return getTodos('mario.json');
+}).then((data) => {
+    console.log('promise 2 resolved:', data);
+    return getTodos('shaun.json'); 
+}).then((data) => {
+    console.log('promise 3 resolved:', data);
+}).catch((err) => {
+    console.log('promise rejected', err);
+});
